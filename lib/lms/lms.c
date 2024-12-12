8c8a702a0c67dcfec45335dce2e890559073a967
Steven Lee <steven_lee@aspeedtech.com>
2024-12-12 17:28:59 +0800

    crypto: Support updated aspeed crypto driver
    Aspeed crypto driver has been refactored to support Zephyr 3.7.0.
    This commit updates the driver name and removes obsolete configurations
    to align with the new driver implementation.
    
    Signed-off-by: Steven Lee <steven_lee@aspeedtech.com>
    Change-Id: Id2237cfa544c92ae2f088a78a4a4c9e3993a5bca

--- lib/lms/lms.c
+++ lib/lms/lms.c
@@ -87,7 +87,7 @@ static const unsigned char D_INTR_CONSTANT_BYTES[D_CONST_LEN] = { 0x83, 0x83 };
 
 #ifdef CONFIG_CRYPTO_ASPEED
 #include <crypto/hash.h>
-#define HASH_DRV_NAME CONFIG_CRYPTO_ASPEED_HASH_DRV_NAME
+#define HASH_DRV_NAME DEVICE_DT_NAME(DT_INST(0, aspeed_hace))    // define hash driver for Aspeed
 static int has_engine_setup = 0;
 static int hw_sha256_abort(const struct device *dev, struct hash_ctx *ini)
 {
